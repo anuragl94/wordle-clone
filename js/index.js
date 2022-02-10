b55a83047fb02e39ef2887ab58c0dec9f0281eb5
Divij Agarwal <divij.agarwal@zopsmart.com>
2022-02-10 15:24:05 +0530

    bug fix

--- js/index.js
+++ js/index.js
@@ -9,21 +9,53 @@ function registerGuess(guess) {
     guess = guess.toUpperCase();
     const status = [];
     const WORD_LETTERS = WORD.split("");
+    const frequency= (array) => {
+        const map = {};
+        array.forEach(item => {
+           if(map[item]){
+              map[item]++;
+           }else{
+              map[item] = 1;
+           }
+        });
+        return map;
+     };
+    const maps=frequency(WORD.split(""));
+    console.log(maps);
+    var k=0;
+    guess.split("").forEach(function(letter, index) {
+        // TODO: handle additional letters when there are duplicates
+        let letterStatus;
+        const isInPlace = WORD_LETTERS[index] === letter;
+        if (isInPlace)  {
+            letterStatus = 2;
+            maps[letter]--;
+            //console.log(maps);
+            status[k++]=2;
+        }
+        else{
+            status[k++]=0;
+        }
+    })
+    console.log(status);
+
     guess.split("").forEach(function(letter, index) {
         // TODO: handle additional letters when there are duplicates
         let letterStatus;
         const existsInWord = WORD_LETTERS.indexOf(letter) > -1;
+        
         const isInPlace = WORD_LETTERS[index] === letter;
-        if (isInPlace) {
-            letterStatus = 2;
-        } else if (existsInWord) {
+        
+        if (existsInWord && maps[letter]>0 && status[index]==0) {
             letterStatus = 1;
-        } else {
-            letterStatus = 0;
+            maps[letter]--;
+            status[index]=1
         }
-        status.push(letterStatus);
+        //status.push(letterStatus);
+        
     })
     printGuess(guess, status);
+    
     return status;
 }
 
