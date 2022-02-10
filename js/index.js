0cb6e71a0bc087bf5354724fc4e298025a1cf0ed
atishkushwaha <atish.kushwaha@zopsmart.com>
2022-02-10 16:59:26 +0530

    Logical error solved

--- js/index.js
+++ js/index.js
@@ -1,28 +1,63 @@
 let attempts = 6;
 
-const WORD = WORDS[Math.floor(Math.random() *  WORDS.length)];
+const WORD =WORDS[Math.floor(Math.random() *  WORDS.length)];
 const el = document.querySelector("#guess");
-
 console.log("Target:", WORD);
+
+function getFrequency(string) {
+    var freq = {};
+    for (var i=0; i<string.length;i++) {
+        var character = string.charAt(i);
+        if (freq[character]) {
+           freq[character]++;
+        } else {
+           freq[character] = 1;
+        }
+    }
+
+    return freq;
+}
 
 function registerGuess(guess) {
     guess = guess.toUpperCase();
     const status = [];
-    const WORD_LETTERS = WORD.split("");
-    guess.split("").forEach(function(letter, index) {
-        // TODO: handle additional letters when there are duplicates
-        let letterStatus;
-        const existsInWord = WORD_LETTERS.indexOf(letter) > -1;
-        const isInPlace = WORD_LETTERS[index] === letter;
-        if (isInPlace) {
-            letterStatus = 2;
-        } else if (existsInWord) {
-            letterStatus = 1;
-        } else {
-            letterStatus = 0;
+
+
+    
+
+    var freq = getFrequency(WORD);;
+    console.log(freq);
+    // console.log(freq);
+    // guess.split("").forEach(function(letter, index) {
+    //     // TODO: handle additional letters when there are duplicates
+    //     let letterStatus;
+    //     const existsInWord = WORD_LETTERS.indexOf(letter) > -1;
+    //     const isInPlace = WORD_LETTERS[index] === letter;
+    //     if (isInPlace) {
+    //         letterStatus = 2;
+    //     } else if (existsInWord) {
+    //         letterStatus = 1;
+    //     } else {
+    //         letterStatus = 0;
+    //     }
+    //     status.push(letterStatus);
+    // })
+
+    let letterStatus;
+    for(let i=0;i<guess.length;i++){
+        const isInPlace = WORD[i] === guess[i];
+        if(isInPlace){
+            freq[guess[i]]--;
+            letterStatus=2;
+        }else if(WORD[i]!=guess[i] && freq[guess[i]]>0){
+            freq[guess[i]]--;
+            letterStatus=1;
+        }else{
+            letterStatus=0;
         }
         status.push(letterStatus);
-    })
+    }
+    
     printGuess(guess, status);
     return status;
 }
