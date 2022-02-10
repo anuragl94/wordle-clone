41e485048c81745f720964d11b380203a8429aec
gitapoorv <apoorv.gupta@zopsmart.com>
2022-02-10 17:25:35 +0530

    Bug Resolved

--- js/index.js
+++ js/index.js
@@ -9,20 +9,54 @@ function registerGuess(guess) {
     guess = guess.toUpperCase();
     const status = [];
     const WORD_LETTERS = WORD.split("");
+    const ext = {};
+
+    WORD_LETTERS.map((letter)=>{
+
+        if(ext[letter]){
+            ext[letter] += 1;
+        }
+        else{
+            ext[letter] = 1;
+        }
+    })
+
+   console.log('=>>>>>>>>>>.',ext);
+
+    
     guess.split("").forEach(function(letter, index) {
         // TODO: handle additional letters when there are duplicates
         let letterStatus;
-        const existsInWord = WORD_LETTERS.indexOf(letter) > -1;
         const isInPlace = WORD_LETTERS[index] === letter;
-        if (isInPlace) {
-            letterStatus = 2;
-        } else if (existsInWord) {
-            letterStatus = 1;
-        } else {
-            letterStatus = 0;
+
+        if(isInPlace)
+        {
+             letterStatus=2;
+             status[index]=2;
+             ext[letter]--;
         }
-        status.push(letterStatus);
+        else
+        {
+              letterStatus=0;
+              status[index]=0;
+        }
     })
+    guess.split("").forEach(function(letter, index) {
+        
+        let letterStatus;
+        const isInPlace = WORD_LETTERS[index] === letter;
+
+        const existsInWord =WORD_LETTERS.indexOf(letter)>-1;
+
+        if( status[index]!==2 && existsInWord && ext[letter]>0 )
+        {
+             letterStatus=1;
+             status[index]=0;
+             ext[letter]--;
+        }
+    })
+
+
     printGuess(guess, status);
     return status;
 }
